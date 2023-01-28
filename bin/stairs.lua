a92fef04bf174c1b1b5c2ec31731272f150983d5
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2023-01-28 01:18:24 -0700

    stairs - add option to lay steps in correct orientation

--- bin/stairs.lua
+++ bin/stairs.lua
@@ -1,6 +1,7 @@
 local arguments = require("/lib/args")
 local bore = require("/lib/bore")
 local location = require("/lib/location")
+local move = require("/lib/move")
 local path = require("/lib/path")
 
 local args = arguments.parse({
@@ -10,12 +11,16 @@ local args = arguments.parse({
       { name = "forward", type = "number" },
       { name = "rightWidth", type = "number" },
     },
+    optional = {
+      { name = "steps", type = "string" } -- boolean really
+    }
   },
   {...})
 
 if args.help then
-  print("To build stairs upwards, place turtle at bottom-most left step (assuming all positive values)")
+  print("To build stairs upwards, place turtle at bottom-most left step (assuming all positive values).")
   print("To build stairs downwards, use a negative clearance, and place turtle just below at the top-most, left ceiling block.")
+  print("Use 'stairs y x z true' to place stairs with correct orientation (slower, and stairs must have a block beneath or it will place incorrectly).")
   return
 end
 
@@ -33,11 +38,21 @@ else
 end
 local clearing = ops.main --reset, main, reverse
 
+local layFloor = function ()
+  local heading = location.getHeading()
+  if args.steps then
+    move.turnTo(vector.new(dim.x * dim.y / math.abs(dim.x * dim.y), 0, 0))
+  end
+  turtle.digDown()
+  turtle.placeDown()
+  move.turnTo(heading)
+end
+
 
 local success, error = pcall(function()
   path.horizontalLayer(dim.x, dim.z, function (direction)
     -- ensure the stairs have a floor
-    if clearing == "Up" then turtle.placeDown() end
+    if clearing == "Up" then layFloor() end
 
     -- clear the column
     for i = 1, math.abs(dim.y), 1 do
@@ -46,7 +61,7 @@ local success, error = pcall(function()
     end
 
     -- ensure the stairs have a floor
-    if clearing == "Down" then turtle.placeDown() end
+    if clearing == "Down" then layFloor() end
 
     -- Switch the clearing for next column
     clearing = (clearing == ops.main and ops.reverse) or ops.main
